9957bcf79591de1b14ed60b09776527bebc41da9
Seah Shun Zhou <seahsz@gmail.com>
2024-10-17 10:12:40 +0800

    initial commit

---
+++ src/Task1/App.java
@@ -0,0 +1,20 @@
+package Task1;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Thread th = new Thread(new Runnable() {
+
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + 
+                    " -> " + i);
+                }
+            }
+        }
+        );
+
+        th.start();
+    }
+}
